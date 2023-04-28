a91a5d80ad03f208450b0760fbcf98d71ff95deb
kpg-anon <128227694+kpg-anon@users.noreply.github.com>
2023-04-27 23:40:34 -0400

    added ilovexs.com scraper

---
+++ shell scripts/xsdl.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Album scraper for ilovexs.com
+
+usage() {
+  echo "Usage: $0 [-a links.txt] <url>"
+  echo "  -a links.txt: specify a txt file containing urls to scrape"
+  echo "  -h: display this help message"
+  exit 1
+}
+
+trap "exit" INT
+while getopts ":a:h" opt; do
+  case ${opt} in
+    a )
+      urls_file=$OPTARG
+      ;;
+    h )
+      usage
+      ;;
+    \? )
+      echo "Invalid option: -$OPTARG" 1>&2
+      usage
+      ;;
+    : )
+      echo "Option -$OPTARG requires an argument." 1>&2
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+if [ -z "$urls_file" ]; then
+  if [ "$#" -ne 1 ]; then
+    usage
+  fi
+  url="$1"
+  urls=("$url")
+else
+  urls=($(cat "$urls_file"))
+fi
+
+for url in "${urls[@]}"; do
+  # Get album name from title tag
+  title=$(curl -s "$url" | sed -n 's/.*<title>\(.*\)<\/title>.*/\1/p')
+  album=$(echo "$title" | sed 's/[[:space:]]*$//')
+
+  # Create directory for images
+  mkdir -p "$album"
+
+  # Scrape images with aria2c
+  curl -s "$url" \
+  | grep -Eo "https://[^/]+/wp-content/uploads/[0-9]{4}/[0-9]{2}/[^/]+\.(jpg|jpeg|webp|png|gif)" \
+  | uniq \
+  | aria2c -i - -c -j 1 -d "$album" 
+done
