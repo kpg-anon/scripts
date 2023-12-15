f2e43eba91c32145168ca22f3356ccf02a82c32d
kpg-anon <128227694+kpg-anon@users.noreply.github.com>
2023-12-15 04:40:39 -0500

    added kastdenquickdl.js

---
+++ userscripts/kastdenquickdl.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Kastden Quick Download
+// @version      1.0
+// @description  Download images by pressing 'S' when hovering over a thumbnail
+// @author       kpganon
+// @match        https://selca.kastden.org/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let hoveredElement = null;
+
+    const downloadImage = (url) => {
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = url.split('/').pop();
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    };
+
+    document.addEventListener('mouseover', function(e) {
+        if (e.target.closest('.image a')) {
+            hoveredElement = e.target.closest('.image a');
+        }
+    });
+
+    document.addEventListener('keydown', function(e) {
+        if (e.key === 's' || e.key === 'S') {
+            if (hoveredElement && hoveredElement.href) {
+                e.preventDefault();
+                downloadImage(hoveredElement.href);
+            }
+        }
+    });
+})();
