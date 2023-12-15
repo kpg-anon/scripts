f0eba2d933a47e48c7022d7774a39d5180801dda
kpg-anon <128227694+kpg-anon@users.noreply.github.com>
2023-12-15 05:08:01 -0500

    added daum_cafe_no_login.user.js

---
+++ userscripts/daum_cafe_no_login.user.js
@@ -0,0 +1,27 @@
+﻿// ==UserScript==
+// @name          Open daum cafe posts without logging in
+// @namespace     https://gitlab.com/loopvid/scripts
+// @description   Open daum cafe posts without logging in
+// @include       /^https?://(m\.)?cafe\.daum\.net/.+/\d+(\?.*)?$/
+// @exclude       /^https?://(m\.)?cafe\.daum\.net/.+/\d+\?svc=cafeapp$/
+// @version       1.0
+// @grant         none
+// @run-at        document-start
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function redirect() {
+    var url = window.location.href.replace(/\?.*$/, '') + '?svc=cafeapp';
+
+    if (url !== window.location.href) {
+      window.location.replace(url);
+    }
+  }
+
+  // run immediately since we want to redirect as soon as possible to avoid
+  // loading the page for no reason
+  redirect();
+
+}());
